9641e963ae28533c4e5433cf0558473bfc6e7c82
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2020-05-19 00:37:05 -0700

    added global input management system

--- main.lua
+++ main.lua
@@ -32,6 +32,8 @@ function love.load()
         fullscreen = false,
         resizable = true
     })
+
+    love.keyboard.keysPressed
 end
 
 function love.resize(w,h)
@@ -39,9 +41,14 @@ function love.resize(w,h)
 end
 
 function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
     if key == 'escape' then 
         love.event.quit()
     end
+end
+
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
 end
 
 function love.update(dt)
@@ -49,6 +56,8 @@ function love.update(dt)
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
     bird:update(dt)
+
+    love.keyboard.keysPressed = {}
 end
 
 function love.draw()
