ccb65a80d77ca2abf861cea5995c94d39453f041
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2020-05-19 00:00:58 -0700

    added paralax scrolling

--- main.lua
+++ main.lua
@@ -9,9 +9,16 @@ VIRTUAL_HEIGHT = 288
 local background = love.graphics.newImage('assets/images/background.png')
 local ground = love.graphics.newImage('assets/images/ground.png')
 
+local backgroundScroll = 0
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
 
 function love.load()
-    
+
     love.window.setTitle("Fifty Bird")
 
     love.graphics.setDefaultFilter('nearest','nearest')
@@ -33,10 +40,15 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+end
+
 function love.draw()
     push:start()
-    love.graphics.draw(background, 0, 0)
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    love.graphics.draw(background, -backgroundScroll, 0)
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
     push:finish()
 end
 
